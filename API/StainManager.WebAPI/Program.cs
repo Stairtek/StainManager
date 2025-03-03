953a1a4a5759812516de17efc33937daaa3cef89
Mike Campbell <mike@stairtek.com>
2025-03-03 10:49:27 -0500

    Update connection string handling in Program.cs and modify buildspec for EF Core migrations

--- API/StainManager.WebAPI/Program.cs
+++ API/StainManager.WebAPI/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     {
         throw new InvalidOperationException("Connection string 'DefaultConnection' is not set.");
     }
-    options.UseSqlServer("Data Source=awseb-e-gkdkcruri3-stack-awsebrdsdatabase-bihhtneld34v.crrxev2p5657.us-east-2.rds.amazonaws.com,1433; Initial Catalog=StainManager; Integrated Security=false; User ID=admin; Password=!Cakeja5047;");
+    options.UseSqlServer(Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") ?? 
+                         builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddInfrastructure(builder.Configuration, logger);
